2f97e172b44ac292e7efb1be6065b8afd25c4874
Maliky853 <maliky853@gmail.com>
2022-07-11 11:01:32 +0530

    feat:updated

---
+++ index.js
@@ -0,0 +1,26 @@
+var txtInput = document.querySelector("#txtInput")
+var btnTranslate = document.querySelector("#btnTranslate")
+var outputDiv = document.querySelector("#outputDiv")
+
+let url = "https://api.funtranslations.com/translate/minion.json"
+
+function translationalUrl(text){
+    return url  +  "?" + "text=" + text
+}
+
+function clickHandler(){
+    console.log("clicked")
+    console.log(`input : ${txtInput.value}`)
+}
+
+btnTranslate.addEventListener("click", function clickhandler(){
+    fetch(translationalUrl(txtInput.value))
+    .then((res)=>res.json())
+    .then(function(json){
+        return outputDiv.innerHTML = json.contents.translated
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+})
+clickHandler()
